d6c42f6f551dc0d13b09b701906c45dfc816c632
3782291211 <Laklouk98@hotmail.com>
2022-12-12 11:52:56 +0000

    Adding updated README with instructions on setting up environment variables; adding sql script for querying development database

---
+++ db/scrap.sql
@@ -0,0 +1,13 @@
+\c nc_news;
+
+\echo '\n \n topics \n'
+SELECT * FROM topics LIMIT 10;
+
+\echo '\n \n users \n'
+SELECT * FROM users LIMIT 10;
+
+\echo '\n \n articles \n'
+SELECT * FROM articles LIMIT 10;
+
+\echo '\n \n comments \n'
+SELECT * FROM comments LIMIT 10;
