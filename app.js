9549b915497a3294dd95a785f002b077233b3095
3782291211 <Laklouk98@hotmail.com>
2023-01-09 10:36:30 +0000

    applying cors middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const apiRouter = require('./routes/api-router');
 const {invalidPath, handlePSQLerrors, handleForeignKeyError, handleCustomErrors, handle500errors} = require('./controllers/news.controllers.errors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(apiRouter);
 app.all('*', invalidPath);
