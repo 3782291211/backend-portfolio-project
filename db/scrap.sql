c0157d2cffc7b81467e32c8adf26fca56fa9c769
3782291211 <Laklouk98@hotmail.com>
2023-02-28 16:25:04 +0000

    Testing query structure

--- db/scrap.sql
+++ db/scrap.sql
@@ -1,4 +1,4 @@
-\c nc_news_test;
+\c nc_news;
 
 \echo '\n \n Use this file to pipe query output data into a .txt file for easy viewing. \n'
 
@@ -13,7 +13,7 @@ GROUP BY slug;
 SELECT comment_id, comments.votes, comments.created_at, comments.author, comments.body, avatar_url FROM comments 
 JOIN users 
 ON comments.author LIKE users.username
-ORDER BY created_at DESC;*/
+ORDER BY created_at DESC;
 
 SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, COUNT(comment_id) ::INTEGER AS comment_count 
   FROM articles
@@ -31,4 +31,14 @@ SELECT articles.author, title, articles.article_id, topic, articles.created_at, 
   LEFT OUTER JOIN comments
   ON articles.article_id = comments.article_id
   WHERE topic = 'mitch'
-  GROUP BY articles.article_id, avatar_url ORDER BY topic DESC LIMIT 50 OFFSET 0;
+  GROUP BY articles.article_id, avatar_url ORDER BY topic DESC LIMIT 50 OFFSET 0;*/
+
+
+  SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, COUNT(comment_id) ::INTEGER AS comment_count, avatar_url
+  FROM articles
+  JOIN users
+  ON articles.author = users.username
+  LEFT OUTER JOIN comments
+  ON articles.article_id = comments.article_id
+  WHERE topic = 'coding' AND articles.author = 'tickle122'
+  GROUP BY articles.article_id, avatar_url ORDER BY author ASC LIMIT 50 OFFSET 0;
