82556eee577773e3542f3502af155d5d4bc483c0
3782291211 <Laklouk98@hotmail.com>
2023-03-02 01:37:36 +0000

    Removing code section

--- models/news.models.js
+++ models/news.models.js
@@ -123,12 +123,6 @@ exports.insertUser = (username, password, name, avatar_url) => {
 }
 
 exports.updateUser = (currentUsername, newUsername, password, name, avatar_url) => {
-  for (const element of [newUsername, password, name, avatar_url]) {
-    if (/^\s+$/.test(element)) {
-      return Promise.reject({status: 400, msg: "Field cannot be a whitespace."});
-    };
-  };
-
   return db.query(`
   UPDATE users
   SET 
